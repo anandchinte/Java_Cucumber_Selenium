7ce3ec7f3c7d3695faea9e7cb9cc64b2270890a6
lakshmi-dasari <lakshmi.dasari1992@gmail.com>
2021-10-19 22:34:59 -0400

    first commit

---
+++ src/cucumberOptions/TestRunner.java
@@ -0,0 +1,14 @@
+package cucumberOptions;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/Features/",
+        glue = "StepDefinitions"
+)
+public class TestRunner {
+
+
+}
